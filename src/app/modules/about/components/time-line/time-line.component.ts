dd7af2e74840dbcddfae9aeab2e83a7ea66074d3
minhvo99 <pikachu43dn@gmail.com>
2025-03-15 13:39:18 +0700

    objective model to generate UI

--- src/app/modules/about/components/time-line/time-line.component.ts
+++ src/app/modules/about/components/time-line/time-line.component.ts
@@ -1,9 +1,59 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'section-time-line',
   templateUrl: './time-line.component.html',
   styleUrl: './time-line.component.scss',
 })
-export class TimeLineComponent {}
+export class TimeLineComponent {
+  timeLines = [
+    {
+      time: '1992',
+      content: ` Seit 1992 haben wir mit Coverflex Software unterschiedliche Lösungen
+          erstellt. Über die Jahre haben sich Dinge verändert, Programme sind
+          als APPs in das Internet gestellt und die Technik dahinter ist
+          vielfältiger geworden. Heute betreiben wir oder unsere Partner
+          einzelne Dienste als WebService oder als APP.`,
+    },
+    {
+      time: '2004',
+      content: ` Seit 1992 haben wir mit Coverflex Software unterschiedliche Lösungen
+          erstellt. Über die Jahre haben sich Dinge verändert, Programme sind
+          als APPs in das Internet gestellt und die Technik dahinter ist
+          vielfältiger geworden. Heute betreiben wir oder unsere Partner
+          einzelne Dienste als WebService oder als APP.`,
+    },
+    {
+      time: '2007',
+      content: ` Seit 1992 haben wir mit Coverflex Software unterschiedliche Lösungen
+          erstellt. Über die Jahre haben sich Dinge verändert, Programme sind
+          als APPs in das Internet gestellt und die Technik dahinter ist
+          vielfältiger geworden. Heute betreiben wir oder unsere Partner
+          einzelne Dienste als WebService oder als APP.`,
+    },
+    {
+      time: '2009',
+      content: ` Seit 1992 haben wir mit Coverflex Software unterschiedliche Lösungen
+          erstellt. Über die Jahre haben sich Dinge verändert, Programme sind
+          als APPs in das Internet gestellt und die Technik dahinter ist
+          vielfältiger geworden. Heute betreiben wir oder unsere Partner
+          einzelne Dienste als WebService oder als APP.`,
+    },
+    {
+      time: '2014',
+      content: ` Seit 1992 haben wir mit Coverflex Software unterschiedliche Lösungen
+          erstellt. Über die Jahre haben sich Dinge verändert, Programme sind
+          als APPs in das Internet gestellt und die Technik dahinter ist
+          vielfältiger geworden. Heute betreiben wir oder unsere Partner
+          einzelne Dienste als WebService oder als APP.`,
+    },
+    {
+      time: '2024',
+      content: ` Seit 1992 haben wir mit Coverflex Software unterschiedliche Lösungen
+          erstellt. Über die Jahre haben sich Dinge verändert, Programme sind
+          als APPs in das Internet gestellt und die Technik dahinter ist
+          vielfältiger geworden. Heute betreiben wir oder unsere Partner
+          einzelne Dienste als WebService oder als APP.`,
+    },
+  ];
+}
--- src/app/modules/home/components/integration-section/integration-section.component.ts
+++ src/app/modules/home/components/integration-section/integration-section.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
 @Component({
   selector: 'integration-section',
   templateUrl: './integration-section.component.html',
   styleUrl: './integration-section.component.scss'
 })
 export class IntegrationSectionComponent {
-    imgSection2 = ['integration-1', 'integration-2', 'integration-3', 'integration-4'];
+    integrationItems  = [
+      {
+        title: 'Flexible Softwareintegration',
+        description: ` Sie können jederzeit den Mix von Software & APPs einsetzen, den Sie
+          für Ihr Geschäft brauchen.`,
+        img: 'assets/images/integration-1.jpg',
+        effect: 'fade-right'
+      },
+      {
+        title: 'Nahtlose technische Harmonie am Arbeitsplatz',
+        description: ` Mobile Geräte, Computer, Betriebssysteme und Anwendungen arbeiten wie
+          ein Orchester zusammen, wenn Sie Ihre Arbeit im Unternehmen erledigen.`,
+        img: 'assets/images/integration-2.jpg',
+        effect: 'fade-up'
+      },
+      {
+        title: 'Optimieren Sie Ihre Arbeitsabläufe mit Coverflex',
+        description: `Mit einems
+          <strong style="color: #aacccc">Coverflex Project Konto</strong> können
+          Sie auf Ihrem PC eigene <strong>Profile</strong> zur Steuerung von
+          <strong>Arbeitsabläufen</strong> erstellen. Sie koordinieren den
+          Umlauf und Fluß von Dokumenten, ihre Be- und Verarbeitung und Ablage.
+          Hierfür können Sie vorgefertigte APPs nutzen - von vielen Anbietern
+          über deren APIs oder Import-/Export-Funktionen.`,
+        img: 'assets/images/integration-3.jpg',
+        effect: 'fade-left'
+      }
+    ]
   }
